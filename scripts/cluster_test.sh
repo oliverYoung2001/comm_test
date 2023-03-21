46eb67b0be7e9c52e81ec9fe558ac2eedc70ea6b
oliverYoung2001 <2937266700@qq.com>
2023-03-21 15:19:13 +0800

    delete results&&scripts

--- scripts/cluster_test.sh
+++
@@ -1,2 +0,0 @@
-srun -N 2 --ntasks-per-node 1 -w nico3,nico4 \
-    sh scripts/cluster_test_inner.sh
--- scripts/ring_test.sh
+++
@@ -1,4 +0,0 @@
-CUDA_LAUNCH_BLOCKING=1 \
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python ring_test.py \
-    --gpus 4
