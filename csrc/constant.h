13eb764f09beb37d3ac0ac286501634a95f07b7e
oliverYoung2001 <2937266700@qq.com>
2024-02-20 16:44:00 +0800

    add some scripts

---
+++ csrc/constant.h
@@ -0,0 +1,8 @@
+#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+// #define CHECK_RESULT
+// #define PRINT_JSON
+// #define DIFF_BUF        // use different buffer for each p2p
+
+#endif
--- scripts/env_octave.sh
+++ scripts/env_octave.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/env.sh
-spack load cuda@12.2.1%gcc@12.2.0
 spack load openmpi
+spack load cuda@12.2.1%gcc@12.2.0
+
